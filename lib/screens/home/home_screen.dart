aaea8d5ae57e9ecb671f37fb78810bc1fc09c581
isro24 <alamat_email_anda@example.com>
2025-05-22 13:48:46 +0700

    Menambah AppBar pada HomeScreen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: buildAppBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+    leading: IconButton(
+      onPressed: (){}, 
+      icon: SvgPicture.asset("assets/icons/menu.svg")),
+    );
   }
 }
